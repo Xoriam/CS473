e4ce18d8efb05ab1dd0e7c7656009bb20cce4a53
Mario Andrade <masterxoriam@gmail.com>
2015-03-04 11:59:56 -0800

    Add fizzbuzz_3, where the function takes in an array and verifies which values are Fizz, Buzz, FizzBuzz, or None

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -16,26 +16,32 @@ var main = function () {
 	};
 
 	$fizzbuzz_1 = function () {
-		console.log("FizzBuzz 1:\n");
+		console.log("FizzBuzz 1:");
 		for(var i = 1; i <= 100; i++) {
 			$verify(i);
 		}
 	};
 
-	$fizzbuzz_2 = function(start, end) {
-		console.log("FizzBuzz 2:\n");
+	$fizzbuzz_2 = function (start, end) {
+		console.log("FizzBuzz 2:");
 		for (var i = start; i <= end; i++) {
 			$verify(i);
 		}
 	};
 
-	$fizzbuzz_3 = function
+	$fizzbuzz_3 = function (arr) {
+		console.log("Fizzbuzz 3:");
+		arr.forEach(function (value) {
+			$verify(value);
+		});
+	}
 
 
 
 
-	$fizzbuzz_1();
-	$fizzbuzz_2(200,300);
+	//$fizzbuzz_1();
+	//$fizzbuzz_2(200,300);
+	$fizzbuzz_3([101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115]);
 };
 
 $(document).ready(main);
