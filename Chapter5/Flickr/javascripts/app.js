e03eba6444521d37fe9893f0c7c37014fc2427e5
Mario Andrade <masterxoriam@gmail.com>
2015-03-10 23:57:02 -0700

    Flickr App skeleton created

---
+++ Chapter5/Flickr/javascripts/app.js
@@ -0,0 +1,11 @@
+var main = function () {
+	"use strict";
+
+	/*This is actually just one string,
+	  but I spread it out over two lines
+	  to make it readable*/
+	  
+	console.log("Hello World!");
+};
+
+$(document).ready(main);
