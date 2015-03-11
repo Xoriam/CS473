fe6a8293bb709ec221b8978e0653415a88ba74b4
Mario Andrade <masterxoriam@gmail.com>
2015-03-11 00:04:05 -0700

    Flickr responds with JSON based off the tag for dogs.

--- Chapter5/Flickr/javascripts/app.js
+++ Chapter5/Flickr/javascripts/app.js
@@ -1,11 +1,17 @@
 var main = function () {
 	"use strict";
 
-	/*This is actually just one string,
-	  but I spread it out over two lines
-	  to make it readable*/
-	  
-	console.log("Hello World!");
+	//This is actually just one string,
+	//but I spread it out over two lines
+	//to make it readable
+
+	var url = "http://api.flickr.com/services/feeds/photos_public.gne?" + "tags=dogs&format=json&jsoncallback=?";
+
+	$.getJSON(url, function (flickrResponse) {
+		//We'll simply print the response to the console
+		//for the time being
+		console.log(flickrResponse);
+	});
 };
 
 $(document).ready(main);
